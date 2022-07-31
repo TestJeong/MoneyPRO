86e8c44758f6912517046557cfc8b5be1fdeef9d
Jeong <jyunj3015@gmail.com>
2022-07-31 22:11:28 +0900

    Add : prisma DB 연결
    - mysql local DB 연결

--- pages/api/search/koreanstock.ts
+++ pages/api/search/koreanstock.ts
@@ -1,19 +1,29 @@
 import type {NextApiRequest, NextApiResponse} from "next"
 import koreaStock from "../../../json/koreaStock.json"
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 type Data = {
   name: string
 }
 
-function koreanStockList(req: NextApiRequest, res: NextApiResponse<any>) {
+async function koreanStockList(req: NextApiRequest, res: NextApiResponse<any>) {
   const {stockName} = req.query
-
   const result = koreaStock.filter((data) => {
     if (stockName === "") return
     return data.stock_name.includes(stockName as string)
   })
-
   res.status(200).json(result.slice(0, 20))
+
+  // await prisma.user.create({
+  //   data: {
+  //     email: "ddd",
+  //     name: "asdfasdfasdf"
+  //   }
+  // })
+
+  // res.json({ok: true})
 }
 
 export default koreanStockList
